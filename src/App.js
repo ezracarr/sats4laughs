b93c18225dd288f22e7b265ec68883d6d6096081
Andrew Fink <andrewfink@Andrews-MacBook-Pro.local>
2022-08-21 17:04:45 -0500

    adding typewriter effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import "./styles.css";
 import JokeDashbaord from './components/JokeDashboard';
+import Typewriter from 'typewriter-effect';
 
 function App() {
+  const typeWriter = new Typewriter('#typewriter', {
+    strings: ['Click on a joke to pay for the punchline'],
+    autoStart: true,
+    loop: false,
+  });
   return (
     <div className="App">
       <h1>Sats-4-Laughs</h1>
-      <h2>Click on a joke to pay for the punchline</h2>
+      <h2>
+        <Typewriter
+            options={{
+              strings: ['Click on a joke to pay for the punchline'],
+              autoStart: true,
+              loop: false,
+              deleteAll: 1,
+              cursor: '',
+              deleteSpeed: 33,
+              delay: 33,
+              deleteChars: 5,
+            }}
+        />
+      </h2>
       <JokeDashbaord />
     </div>
   );
