a3351e1fc828591041d5ed060016ddb3c79a3860
Andrew Fink <andrewfink@Andrews-MacBook-Pro.local>
2022-08-21 15:42:16 -0500

    mash integrated

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -13,7 +13,14 @@ const Joke = (props) => {
       };
     const payForJoke = () => {
         console.log('here')
-        setPaid(true)
+        window.Mash.hasAccess("c9acc1c1-8d0e-45e1-9628-202b5ef1f893").then(granted => {
+            if (granted) {
+              setPaid(true)
+              flipBack()
+              return
+            }
+          })
+        // window.Mash.hasAccess("c9acc1c1-8d0e-45e1-9628-202b5ef1f893")
         // axios.get("https://jsonplaceholder.typicode.com/users").then(response => {
         //         setPaid(true)
         // })
@@ -29,7 +36,7 @@ const Joke = (props) => {
           flipDirection="vertical"
           flipSpeedBackToFront={0.5}
         >
-          <div className="card card-front" onClick={flipBack}>
+          <div className="card card-front" onClick={payForJoke}>
             {props.jokeData.setup}
           </div>
           <div className="card card-back" onClick={flipFront}>
